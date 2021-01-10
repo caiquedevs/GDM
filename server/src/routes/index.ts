cc46d90e109517b1202f657b63b13c81bc026e5f
Caique Lourenço <caique.lourenco@modalgr.com.br>
2021-01-10 01:55:52 -0300

    Configurou Sequelize

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,16 @@
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import doctors from './doctor.routes';
+import specialty from './specialty.routes';
+
+const corsOptions = {
+  origin: 'http://localhost:8081',
+};
 
 export default function routes(app: any) {
-  app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
-  app.use(doctors);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cors(corsOptions));
+  app.use(doctors, specialty);
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express';
 import routes from './routes';
 
 const app = express();
+
 routes(app);
 
 const port = process.env.PORT || 3333;
