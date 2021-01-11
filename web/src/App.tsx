abfdba9fcf130f45a6709bd365edf01656d83093
Caique Lourenço <caique.lourenco@modalgr.com.br>
2021-01-11 04:45:47 -0300

    Configurou index e app do projeto web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import GlobalStyle from './styles/styled';
+import HomePage from './pages';
+import api from './services/api';
+
+function App(): JSX.Element {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      await api
+        .get('/doctors')
+        .then(response => { setDoctors(response.data.reverse()) })
+        .catch(error => { console.log(error) })
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      Ola mundo
-    </div>
+    <>
+      <GlobalStyle />
+      <HomePage doctors={doctors} setDoctors={setDoctors} />
+    </>
   );
 }
 
